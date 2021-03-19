f6e4ebacf668c48d154843df87adcd5cf6773a6c
Trent Renden <trent_renden@hotmail.com>
2021-03-19 20:19:05 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+
+const newHeader = document.createElement("H1")       
+newHeader.id = 'victory';
+const t = document.createTextNode("Trent is the champion");    
+newHeader.appendChild(t);  
+
